8b4e59a531e6cbccc5673e01e7acaeb165e2c978
Realhomelover <jae089265@gmail.com>
2024-04-08 21:57:48 +0900

    "chore: 이해를 돕기 위한 주석 추가"

--- 2509.cpp
+++ 2509.cpp
@@ -2,14 +2,14 @@
 using namespace std;  
 int a[9]; 
 int main() {
-    ios_base::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false); // 속도 증가 목적
     cin.tie(NULL);
     cout.tie(NULL);
     for(int i = 0; i < 9; i++){
         cin >> a[i]; 
     }
     sort(a, a + 9);
-    do{
+    do{ //do-while permutation
         int sum = 0; 
         for(int i = 0; i < 7; i++) sum += a[i]; 
         if(sum == 100)break;
